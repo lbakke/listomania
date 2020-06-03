21bef4332d4cd0a407cd208bb5bd69dca8372ba1
Lauren Bakke <lbakke@nd.edu>
2020-06-02 22:16:05 -0500

    Updates

---
+++ precache-manifest.945c911ebc3aaf0f9ed68ea54d79ff8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5231d0416abd5d9cae29e59f9807284c",
+    "url": "/listomania/index.html"
+  },
+  {
+    "revision": "35aa9d2901115edf3978",
+    "url": "/listomania/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c036ab7b28b70e0c3acf",
+    "url": "/listomania/static/js/2.9a1ec2a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/listomania/static/js/2.9a1ec2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35aa9d2901115edf3978",
+    "url": "/listomania/static/js/main.ee7b24cb.chunk.js"
+  },
+  {
+    "revision": "77c5635594ef48163032",
+    "url": "/listomania/static/js/runtime-main.322a134a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/listomania/static/media/logo.5d5d9eef.svg"
+  }
+]);
